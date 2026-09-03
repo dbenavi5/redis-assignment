8960c02fe5532db08060c9c67c76c4145dc31f05
dbenavi5 <dbenavides415@outlook.com>
2026-09-02 21:01:30 -0700

    Deploy FastAPI and Redis to Kubernetes

--- app.py
+++ app.py
@@ -7,17 +7,19 @@ app = FastAPI()
 
 
 # Redis connection settings.
-# 
-# In Module 1, Redis runs directly on out local Mac, so localhost is
-# the correct default.
-# 
-# Later, Kubernetes will override REDIS_HOST and set it to the Redis
-# Kubernetes Service name.
+#
+# Local development defaults to localhost.
+# Docker and Kubernetes override these settings with environment variables.
 REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
+REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
 
 
 # Create the Redis client.
+#
+# At this stage REDIS_PASSWORD is injected into the application so we can
+# demonstrate Kubernetes Secrets, but Redis authentication itself has not
+# been enabled yet.
 r = redis.Redis(
     host=REDIS_HOST,
     port=REDIS_PORT
@@ -26,14 +28,14 @@ r = redis.Redis(
 
 @app.middleware("http")
 async def log_requests(request: Request, call_next):
-    print(f'Incoming request path: {request.url.path}')
+    print(f"Incoming request path: {request.url.path}")
     response = await call_next(request)
     return response
 
 
 @app.get("/")
 def root():
-    return {"message": "FastAPI is working in Docker"}
+    return {"message": "FastAPI is working in Kubernetes"}
 
 
 @app.post("/cache")
@@ -49,10 +51,17 @@ def get_value(key: str):
     if value is None:
         raise HTTPException(
             status_code=404,
-            detail="Ket not found"
+            detail="Key not found"
         )
 
     return {
         "key": key,
         "value": value.decode()
+    }
+
+
+@app.get("/secret-test")
+def show_secret():
+    return {
+        "secret_loaded": REDIS_PASSWORD is not None
     }
